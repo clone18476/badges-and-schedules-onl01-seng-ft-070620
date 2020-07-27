0bad960dfe68dcfe39bef3b43684ee9aab6558a4
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-27 01:38:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -16,8 +16,11 @@ attendees.each do |name|
  end
 return array
 end
+
 def assign_rooms(attendees)
+  
   new_array=[]
+  
 attendees.each_with_index do |name, index |
   new_array.push("Hello, #{name}! You'll be assigned to room #{index +1}!")
  end 
