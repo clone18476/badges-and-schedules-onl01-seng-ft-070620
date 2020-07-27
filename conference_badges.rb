1b6fb0ed220f50707500b52bf98ca3a07ab942d6
Cameron Cousins <cameroncousins18@gmail.com>
2020-07-27 01:37:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,7 +1,11 @@
 badge_maker = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+
 def badge_maker(name)
+  
 return "Hello, my name is #{name}."
     end
+    
 def batch_badge_creator(attendees)
 array=[]
 attendees.each do |name|
